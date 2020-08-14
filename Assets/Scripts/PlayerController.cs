12758854133fc7ef15d48dac25f882d38f28dae2
LeDian01 <775340552@qq.com>
2020-08-14 23:57:57 +0800

    init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float speedMultiple = 1;
+
+    Vector3 mouseWorldPosition;
+
+    Vector2 move;
+    [SerializeField]
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+
+    }
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        move.x = Input.GetAxisRaw("Horizontal");
+        move.y = Input.GetAxisRaw("Vertical");
+        mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPosition.z = 0;
+        //transform.LookAt(mouseWorldPosition, Vector3.right);
+        transform.up =mouseWorldPosition-transform.position;
+
+        rb.velocity = move * speed * speedMultiple;
+    }
+}
---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+
+    public bool isHoldingTrigger;
+
+
+    private void Update()
+    {
+        isHoldingTrigger = Input.GetMouseButton(0);
+    }
+}
+
+public class Gun
+{
+    public virtual void Shoot()
+    {
+
+    }
+}
